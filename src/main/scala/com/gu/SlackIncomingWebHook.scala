0785273babe803f2ebf37b73e04eeb0e1900dfd0
jharewinton <jonathan.hare-winton@guardian.co.uk>
2016-10-05 12:08:53 +0100

    manage client to cleanup threads

--- src/main/scala/com/gu/SlackIncomingWebHook.scala
+++ src/main/scala/com/gu/SlackIncomingWebHook.scala
@@ -7,19 +7,18 @@ import play.api.libs.ws.WSResponse
 import play.api.libs.ws.ahc.AhcWSClient
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class SlackIncomingWebHook(url: String) {
 
   implicit val actorSystem = ActorSystem()
   implicit val materializer = ActorMaterializer()
-  val client = AhcWSClient()
 
   def send(payload: Payload): Future[WSResponse] = {
+    val client = AhcWSClient()
     client
-      .url(url)
-      .withHeaders("Content-type" -> "application/json")
-      .post(Json.toJson(payload))
+     .url(url)
+     .withHeaders("Content-type" -> "application/json")
+     .post(Json.toJson(payload)) andThen { case _ => client.close() }
   }
-
-  //TODO: Manage client
 }
